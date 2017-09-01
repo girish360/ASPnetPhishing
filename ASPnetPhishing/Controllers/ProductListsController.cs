1ff18224143a984e0616e9f6f595d8e47450c6a8
John_Soerjawitaka <johnsoerjawitaka@yahoo.com>
2017-09-01 11:08:09 -0500

    finish product navigation

--- ASPnetPhishing/Controllers/ProductListsController.cs
+++ ASPnetPhishing/Controllers/ProductListsController.cs
@@ -17,11 +17,30 @@ namespace ASPnetPhishing.Controllers
         // GET: ProductLists
         public ActionResult Index()
         {
-            var products = db.Products.Include(p => p.ProductCategory).ToList();
-            var category = db.ProductCategories.ToList();
-            ViewBag.categoryModel = category;
-            return View(products);
+            var category = db.ProductCategories.Where(pc => pc.CategoryName != "Permit").ToList();
+            return View(category);
         }
+
+        // GET: ProductLists/show queried products
+        public ActionResult ProductList(ProductCategory productCategory)
+        {
+            int catId = productCategory.Id;
+            List<ASPnetPhishing.Models.Product> products;
+            if (catId >= 0)
+            {
+                if (catId == 0)
+                {
+                    catId = Convert.ToInt32(Session["category"]);
+                }
+                Session["category"] = catId;
+                products = db.Products.Where(p => p.CategoryId == catId).Include(p => p.ProductCategory).ToList();
+            }
+            else
+            {
+                products = db.Products.Include(p => p.ProductCategory).ToList();
+            }
+            return View(products);
+        } 
 
         // GET: ProductLists/Details/5
         public ActionResult Details(int? id)
